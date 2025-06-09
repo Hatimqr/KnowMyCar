d3a68fdea21a0d86ea28bdb30abbc741075d5f17
Hatim <hatimqr02@gmail.com>
2025-06-10 04:28:29 +0530

    Initial Commit

---
+++ KnowMyCarTests/KnowMyCarTests.swift
@@ -0,0 +1,17 @@
+//
+//  KnowMyCarTests.swift
+//  KnowMyCarTests
+//
+//  Created by Hatim Rehmanjee on 6/10/25.
+//
+
+import Testing
+@testable import KnowMyCar
+
+struct KnowMyCarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
